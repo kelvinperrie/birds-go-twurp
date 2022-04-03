c372ea4f2726710ce36e7896d459c814e54e8b1a
kelvin <kelvinperrie@gmail.com>
2022-04-04 10:45:56 +1200

    adding some night birds + some styling and layout changes

---
+++ scripts/presetconfig.js
@@ -0,0 +1,34 @@
+var presetsConfiguration = [
+    {
+        "title" : "Suburban Backyard",
+        "sounds" : [
+            { "label" : "Blackbird", "volume" : 0.8 },
+            { "label" : "Myna", "volume" : 0.8 },
+            { "label" : "Sparrows", "volume" : 0.5 }
+        ]
+    },
+    {
+        "title" : "Farm",
+        "sounds" : [
+            { "label" : "Pukeko", "volume" : 0.8 },
+            { "label" : "Chaffinch", "volume" : 0.8 },
+            { "label" : "Magpie", "volume" : 0.8 }
+        ]
+    },
+    {
+        "title" : "Bush",
+        "sounds" : [
+            { "label" : "Tieke", "volume" : 0.8 },
+            { "label" : "Grey Warbler", "volume" : 0.3 },
+            { "label" : "Black Cicada", "volume" : 0.7 }
+        ]
+    },
+    {
+        "title" : "Night Camp",
+        "sounds" : [
+            { "label" : "Rain on tent", "volume" : 0.2 },
+            { "label" : "Ruru", "volume" : 1 },
+            { "label" : "NI Brown Kiwi", "volume" : 0.5 }
+        ]
+    }
+];
---
+++ scripts/soundconfig.js
@@ -0,0 +1,154 @@
+var soundsConfiguration = [
+    {
+        "file" : "sounds/Grey Warbler - Dave Holland.mp3",
+        "label" : "Grey Warbler",
+        "initialVolume" : 0.5,
+        "attribution" : {
+            "name" : "dave_holland",
+            "link" : "aaaa",
+            "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
+        }
+    },
+    {
+        "file" : "sounds/Pukeko - Dave Holland.mp3",
+        "label" : "Pukeko",
+        "initialVolume" : 0.5
+    },
+    {
+        "file" : "sounds/Tieke - Dave Holland.mp3",
+        "label" : "Tieke",
+        "initialVolume" : 0.5
+    },
+    {
+        "file" : "sounds/Blackbird - saracoutinho.wav",
+        "label" : "Blackbird",
+        "initialVolume" : 0.5
+    },
+    {
+        "file" : "sounds/Blackbirds - kate_mcalpine.m4a",
+        "label" : "Blackbirds",
+        "initialVolume" : 0.5
+    },
+    {
+        "file" : "sounds/Myna - Soggybottomnz.m4a",
+        "label" : "Myna",
+        "initialVolume" : 0.5,
+        "attribution" : {
+            "name" : "Soggybottomnz",
+            "link" : "aaaa",
+            "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
+        }
+    },
+    {
+        "file" : "sounds/Sparrows - duncanmc42.wav",
+        "label" : "Sparrows",
+        "initialVolume" : 0.5
+    },
+    {
+        "file" : "sounds/Sparrows - saracoutinho.wav",
+        "label" : "More Sparrows",
+        "initialVolume" : 0.5
+    },
+    {
+        "file" : "sounds/Chaffinch - malletrj.m4a",
+        "label" : "Chaffinch",
+        "initialVolume" : 0.5
+    },
+    {
+        "file" : "sounds/Magpie - andrewmaungakotukutuku.mp3",
+        "label" : "Magpie",
+        "initialVolume" : 0.5
+    },
+    {
+        "file" : "sounds/NI Brown Kiwi - 69494291 - linds-nz-altered2.mp3",
+        "label" : "NI Brown Kiwi",
+        "initialVolume" : 0.5,
+        "attribution" : {
+            "name" : "linds-nz",
+            "link" : "https://inaturalist.nz/observations/69494291",
+            "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
+        }
+    },
+    {
+        "file" : "sounds/Ruru - 102334719 - linds-nz.wav",
+        "label" : "Ruru squawk",
+        "initialVolume" : 0.5,
+        "attribution" : {
+            "name" : "linds-nz",
+            "link" : "https://inaturalist.nz/observations/102334719",
+            "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
+        }
+    },
+    {
+        "file" : "sounds/Ruru - 100382629 - andrewmaungakotukutuku-altered.mp3",
+        "label" : "Ruru",
+        "initialVolume" : 0.5,
+        "attribution" : {
+            "name" : "andrewmaungakotukutuku",
+            "link" : "https://inaturalist.nz/observations/100382629",
+            "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
+        }
+    },
+    {
+        "file" : "sounds/Black Cicada - Dave Holland.mp3",
+        "label" : "Black Cicada",
+        "initialVolume" : 0.5,
+        "category" : "Ambient"
+    },
+    {
+        "file" : "sounds/zapsplat_nature_rainfall_light_on_tent.mp3",
+        "label" : "Rain on tent",
+        "initialVolume" : 0.3,
+        "category" : "Ambient",
+        "attribution" : {
+            "name" : "zapsplat.com",
+            "link" : "https://www.zapsplat.com/",
+            "licenseUrl" : "https://www.zapsplat.com/license-type/standard-license/"
+        }
+    },
+    {
+        "file" : "sounds/zapsplat_nature_wind_strong_gusts_blowing_through_trees_and_bushes.mp3",
+        "label" : "Wind in trees",
+        "initialVolume" : 0.2,
+        "category" : "Ambient",
+        "attribution" : {
+            "name" : "zapsplat.com",
+            "link" : "https://www.zapsplat.com/",
+            "licenseUrl" : "https://www.zapsplat.com/license-type/standard-license/"
+        }
+    },
+    {
+        "file" : "sounds/zapsplat_Rain_Light_Thunder_Peel_LOOP.mp3",
+        "label" : "Rain and thunder",
+        "initialVolume" : 0.2,
+        "category" : "Ambient",
+        "attribution" : {
+            "name" : "zapsplat.com",
+            "link" : "https://www.zapsplat.com/",
+            "licenseUrl" : "https://www.zapsplat.com/license-type/standard-license/"
+        }
+    },
+    {
+        "file" : "sounds/zapsplat_west_wolf_Beach_Sea_Waves.mp3",
+        "label" : "Waves",
+        "initialVolume" : 0.5,
+        "category" : "Ambient",
+        "attribution" : {
+            "name" : "zapsplat.com",
+            "link" : "https://www.zapsplat.com/",
+            "licenseUrl" : "https://www.zapsplat.com/license-type/standard-license/"
+        }
+    },
+    {
+        "file" : "sounds/river-trimcrop.mp3",
+        "label" : "River",
+        "initialVolume" : 0.5,
+        "category" : "Ambient",
+        "attribution" : {
+            "name" : "Kelvin Perrie",
+            "link" : "",
+            "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
+        }
+    }
+    
+];
