63ede4bf7ba5c427d41de8fd2138ed9c2e1aed16
kelvin <kelvinperrie@gmail.com>
2022-04-08 11:28:35 +1200

    adding comments and removing console.logs

--- scripts/birdy.js
+++ scripts/birdy.js
@@ -3,15 +3,15 @@
 var pageModel = function(soundsConfiguration, presetsConfiguration) {
     var self = this;
 
-    self.soundsConfiguration = soundsConfiguration;
-    self.presetsConfiguration = presetsConfiguration;
-    self.soundControllers = ko.observableArray();
+    self.soundsConfiguration = soundsConfiguration;     // holds a colleciton of sound configuration data
+    self.presetsConfiguration = presetsConfiguration;   // holds a collection of preset configuration data
+    self.soundControllers = ko.observableArray();       // holds a collection of sound controller objects created from the sound configuration data
 
+    // create sound controller objects based on our configuration
     self.loadSounds = function() {
         self.soundControllers([]);
         var lastCategory = null;
         for (var soundConfig of self.soundsConfiguration) {
-            console.log(soundConfig);
             var soundController = new soundControllerModel(soundConfig);
             if(lastCategory !== soundController.category()) {
                 soundController.showCategory(true);
@@ -21,21 +21,21 @@ var pageModel = function(soundsConfiguration, presetsConfiguration) {
         }
     }
 
+    // stops all sounds
     self.resetSounds = function() {
         for (var soundController of self.soundControllers()) {
             soundController.resetSound();
         }
     }
 
+    // activate the sounds for a preset
     self.loadPreset = function(preset) {
-        console.log(preset);
+        // stop all sounds
         self.resetSounds();
+        // search for each sound and turn it on
         for (var presetSound of preset.sounds) {
-            console.log("looking for presetSound " + presetSound.label)
             for(var soundController of self.soundControllers()) {
-                console.log("checking " + soundController.label())
                 if(soundController.label() === presetSound.label) {
-                    console.log("found!")
                     soundController.selectedVolume(presetSound.volume * 100)
                     soundController.toggleActive();
                     break;
@@ -53,21 +53,22 @@ var pageModel = function(soundsConfiguration, presetsConfiguration) {
 var soundControllerModel = function(soundConfig) {
     var self = this;
 
-    self.soundConfig = soundConfig;
-    self.ready = ko.observable(false);
-    self.active = ko.observable(false);
-    self.seekWhenStopped = 0;
+    self.soundConfig = soundConfig;         // the configuration that defines this sound
+    self.ready = ko.observable(false);      // tracks whether this sound is ready to be played
+    self.active = ko.observable(false);     // tracks if the sound is active i.e. being played
+    self.seekWhenStopped = 0;               // the point in the sound where it was last stopped
 
-    self.category = ko.observable();
-    self.showCategory = ko.observable(false);
-    self.label = ko.observable();
-    self.file = ko.observable();
-    self.volume = ko.observable();
-    self.selectedVolume = ko.observable();
-    self.initialVolume = ko.observable();
-    self.howlerObject = null;
-    self.attribution = ko.observable();
+    self.category = ko.observable();            // the category, if any, of the sound. Currently only 'ambient' and everything not ambient
+    self.showCategory = ko.observable(false);   // when we're drawing the sound controller on the page do we need to show the category first?
+    self.label = ko.observable();               // the name of the sound
+    self.file = ko.observable();                // path to the sound file
+    self.volume = ko.observable();              // the current volume of the sound; 0 to 1
+    self.selectedVolume = ko.observable();      // this is bound to the slider; 0 to 100
+    self.initialVolume = ko.observable();       // the volume that the sound starts at - used when reseting sounds
+    self.howlerObject = null;                   // the howler object that plays the sound
+    self.attribution = ko.observable();         // an object describing who owns the sound + license etc
 
+    // determines the class for our sound
     self.containerClasses = ko.computed(function() {
         var classes = "sound-container";
         if(self.active()) {
@@ -76,6 +77,7 @@ var soundControllerModel = function(soundConfig) {
         return classes;
     });
 
+    // stop the sound and reset the volume to the default
     self.resetSound = function() {
         self.stopSound();
         self.selectedVolume(self.initialVolume() * 100);
@@ -84,7 +86,6 @@ var soundControllerModel = function(soundConfig) {
     self.stopSound = function() {
         // we're going to stop the sound, remembering where we stopped it
         self.seekWhenStopped = self.howlerObject.seek();
-        console.log(self.seekWhenStopped);
         self.howlerObject.stop();
         self.active(false);
     };
@@ -98,39 +99,37 @@ var soundControllerModel = function(soundConfig) {
 
     self.toggleActive = function() {
         if(!self.ready()) {
-            console.log("can't do anything, sound ain't ready yet");
+            console.log("can't do anything, sound ain't ready yet - this a problem");
             return;
         }
-        console.log("guess we're ready")
         if(self.active()) {
             self.stopSound();
         } else {
-            // play the sound, play if from where ever it was last stopped
+            // play the sound; play it from where ever it was last stopped
             // when a seek is completed play() will be called
             self.howlerObject.seek(self.seekWhenStopped);
         }
     }
 
     self.setupHowler = function() {
-        console.log("setting up howler for " + self.label())
-        console.log("init vol is " + self.volume())
         var sound = new Howl({
             src: [self.file()],
             loop: true,
             volume: self.volume(),
             onload : function() {
-                console.log("onload called")
+                // the sound is loaded, so we're going to set the ready flag which indicates we can play it when wanted
                 self.ready(true);
             },
             onstop : function() {
-                console.log('stopped!');
+                //console.log('stopped!');
             },
             onseek: function() {
-                console.log("onseek called")
+                // when seek is called the sound is ready to be played
                 self.howlerObject.play();
                 self.active(true);
             }
         });
+        // store the howler object so we can manipulate the sound later
         self.howlerObject = sound;
     }
 
